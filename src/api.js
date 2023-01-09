c4d88a33c5a7fae7682d6d4b65687bd35b0a0de3
Daniel Pantea <dani.pantea@gmail.com>
2023-01-09 14:33:22 +0200

    update 09-01-23 for testing5

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 // const urlurl = "http://localhost:3000/data"
 
 
-const urlurl = "https://my-json-server.typicode.com/DaniPantea99/react-todo-app-db"
+const urlurl = "https://my-json-server.typicode.com/DaniPantea99/react-todo-app-db/db"
 
 
 
