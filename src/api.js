5fcb1da9e0822e766e67ecddb93ab1fc3dfc62a7
Daniel Pantea <dani.pantea@gmail.com>
2023-01-09 14:30:14 +0200

    update 09-01-23 for testing4

--- src/api.js
+++ src/api.js
@@ -1,5 +1,10 @@
 // const urlurl = "https://my-json-server.typicode.com/DaniPantea99/react-todo-app/data"
-const urlurl = "http://localhost:3000/data"
+// const urlurl = "http://localhost:3000/data"
+
+
+const urlurl = "https://my-json-server.typicode.com/DaniPantea99/react-todo-app-db"
+
+
 
 
 export default urlurl
